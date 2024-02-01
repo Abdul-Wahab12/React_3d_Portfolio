db67f7ae84744173dd31ffd95076f04bab9ee338
Abdul Wahab <abdulwahabsoftware1@gmail.com>
2024-02-01 21:24:47 +0500

    Added: add complete contact section with form and Earth motion design and set functionality for form via email.js lib

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,7 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
 const Contact = () => {
   const formRef = useRef();
   const [ form, setForm ] = useState({
@@ -15,11 +16,46 @@ const Contact = () => {
   });
   const [ loading, setLoading ] = useState(false);
   const handleChange = (e) => {
+    const { name, value } = e.target;
 
+    setForm({ ...form, [name]: value });
   };
 
   const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
 
+    // template_uuqzevr
+    // service id: service_z8bcjwp
+    // public key: q6oR6thEciiucviaZ
+    emailjs.send(
+        'service_z8bcjwp', 
+        'template_uuqzevr',
+        {
+          from_name: form.name,
+          to_name: 'Abdul Wahab',
+          from_email: form.email,
+          to_email: 'abdulwahabsoftware1@gmail.com',
+          message: form.message
+        },
+        'q6oR6thEciiucviaZ'
+      )
+      .then(() => {
+        setLoading(false);
+        alert('Thank you. I will get back to you as soon as possible.');
+
+        setForm({
+          name: '',
+          email: '',
+          message: ''
+        })
+      }, (error) => {
+        setLoading(false);
+
+        console.log("Error: ",error);
+        
+        alert('Something went wrong.')
+      })
   };
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
@@ -46,7 +82,46 @@ const Contact = () => {
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
+
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input 
+              type="email" 
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea 
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <button 
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {
+              loading ? 'Sending...' : 'Send'
+            }
+          </button>
         </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   )
